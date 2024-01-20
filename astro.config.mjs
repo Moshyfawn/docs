39193292ef7f3a92172f7962d704afbbcd5ce859
moshyfawn <email@moshyfawn.dev>
2024-01-20 16:42:16 -0500

    🚀 init: experimental zed documentation

---
+++ astro.config.mjs
@@ -0,0 +1,45 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Zed',
+			logo: {
+				dark: './src/assets/zed-logo.webp',
+				light: './src/assets/zed-logo-dark.png'
+			},
+			favicon: '/favicon.png',
+			social: {
+				github: 'https://github.com/zed-industries/community',
+			},
+			sidebar: [
+				{
+					label: 'General',
+					autogenerate: { directory: 'general' }
+				},
+				{
+					label: 'Configuration',
+					autogenerate: { directory: 'configuration' }
+				},
+				{
+					label: 'Languages',
+					autogenerate: { directory: 'languages' }
+				},
+				{
+					label: 'Community',
+					autogenerate: { directory: 'community' }
+				}
+			],
+			lastUpdated: true,
+			editLink: { baseUrl: 'https://github.com/zed-industries/docs/edit/main/' },
+			locales: {
+				root: {
+					label: 'English',
+					lang: 'en'
+				}
+			}
+		}),
+	],
+});
---
+++ src/content/docs/community/example.md
@@ -0,0 +1,10 @@
+---
+title: Example Page
+description: This is an example page.
+---
+
+This is an example page.
+
+## Example Heading
+
+- Example list item
